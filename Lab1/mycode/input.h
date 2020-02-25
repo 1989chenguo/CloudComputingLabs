a7916ff135a9fb124c81823fc0b3832a0fbe994c
FengYangyang <fengyangyang@hnu.edu.cn>
2020-02-25 19:47:31 +0800

    the basic structure

---
+++ Lab1/mycode/input.h
@@ -0,0 +1,11 @@
+#include <string>
+#include "global.h"
+
+// store the file content in buffer for the solver to read them one by one
+stirng problemBuffer[PROBLEM_BUFFER_LEN];
+
+// a dedicated thread hold the program for reading content from files
+void ReadController();
+
+// each file read by a thread 
+void ReadThread(FILE filename);
---
+++ Lab1/mycode/sudoku_sover.h
@@ -0,0 +1,14 @@
+#include "global.h"
+#include <string>
+
+stirng resultBuffer[RESULT_BUFFER_LEN];
+
+// the program to solve one part of the problem
+void SudokuSolverHelper(string);
+
+
+// the program to solve the single problem
+void SudokuSolver();
+
+
+void SudokuResult();
